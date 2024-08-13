67b3e345915b59dd1fac82dc4b087b70b1053fba
Koen Draijer <124170375+KMdraijer@users.noreply.github.com>
2024-08-13 15:33:47 +0200

    Add files via upload

--- submitscript/ase_run.py
+++ submitscript/ase_run.py
@@ -3,11 +3,42 @@ from ase.calculators.vasp import Vasp
 from ase.io import read
 import os
 import subprocess
+import glob
+import numpy as np
+import math
+
+def auto_correct_path(path_string):
+    # Check if the path string starts with a "/"
+    if not path_string.startswith('/'):
+        path_string = '/' + path_string
+    
+    # Check if the path string ends with a "/"
+    if not path_string.endswith('/'):
+        path_string = path_string + '/'
+    return path_string
+
+def directory_maker(path_input):
+    input_type = type(path_input)
+    if input_type == str:
+        path = auto_correct_path(path_input)
+        if not os.path.exists(path):
+            os.mkdir(path)
+            print(f'Making {path}')
+        else:
+            print(f'{path} exists')
+    elif input_type == list:
+        for i in path_input:
+            path = auto_correct_path(i)
+            if not os.path.exists(path):
+                os.mkdir(path)
+                print(f'Making {path}')
+            else:
+                print(f'{path} exists')
 
 def submit_vasp_calculation(ase_atoms_object,path_to_files,slurm=False,tasks=32,time_cap='01:00:00', partition = 'genoa'):
     origin = os.getcwd()
     # get template for the submission script
-    templatefile = os.getenv("VASP_SLURM_TEMPLATE")
+    templatefile = os.getenv("LORENTZ_SLURM_TEMPLATE")
     with open(templatefile, "r") as file:
         template = file.read()
     # add the chemical formula of the material as jobname in the submission script
@@ -23,56 +54,58 @@ def submit_vasp_calculation(ase_atoms_object,path_to_files,slurm=False,tasks=32,
         subprocess.run(['sbatch','submit.sh'])
     os.chdir(origin)
     
-# Read your structure (replace 'your_structure.xyz' with your actual structure file)
-atoms = read('your_structure.cif')
+path = glob.glob('/path/to/CO2_adsorption_example/structures/*/*/*.POSCAR')
 
-# Create a VASP calculator
-calc = Vasp(
-    # General VASP parameters
-    xc='RPBE',        # Changed to RPBE
-    encut=400,        # Confirmed cutoff energy of 400 eV
-    istart=0,
-    icharg=2,
-    prec='Accurate',
-    ediff=1e-5,
-    nelm=100,
-    nelmin=6,
-    
-    # K-point density
-    kspacing=0.03,    # This corresponds to KDENS=0.03
-    
-    # Ionic relaxation parameters
-    nsw=0,
-    ibrion=-1,
-    isif=2,
-    ediffg=-0.01,
-    
-    # DFT-D3 correction
-    ivdw=11,
-    
-    # Electric field parameters
-    efield=0.1,
-    ldipol=True,
-    idipol=3,
-    
-    # Constant potential parameters
-    bpotim=0.1,
-    smass=0,
-    nelect=100,  # Adjust this to your system's total number of electrons
-    
-    # Output options
-    lwave=False,
-    lcharg=True,
-    
-    # Parallelization
-    npar=4,
-    
-    # Additional ASE-specific parameters
-    directory='vasp_run',
-    txt='vasp.out'
-)
+for f in path:
+    atoms = read(f,format='vasp')
+    a,b,c,alpha,beta,gamma = atoms.cell.cellpar()
+    ka = round(40/a)
+    kb = round(40/b)
+    kc = 1
+    direct = '/'.join(f.split('/')[0:-1])
+    file = f.split('/')[-1].split('.')[0]
+    for potential in np.linspace(-0.5, 0.5, 11):
+        work = f'{direct}/{file}_potential_{round(potential,2)}eVA/'
+        directory_maker(work)
+        os.chdir(work)
+        atoms.calc = Vasp(xc='RPBE',
+                          encut=400,
+                          kpts = [ka,kb,kc],
+                          istart=0,
+                          icharg=2,
+                          prec='Accurate',
+                          ediff=1e-5,
+                          nelm=100,
+                          nelmin=6,
+                          nsw=0,
+                          ibrion=-1,
+                          isif=2,
+                          ediffg=-0.01,
+                          ivdw=11,
+                          
+                          # Electric field parameters
+                          efield=potential,  # Applied electric field
+                          ldipol=True,
+                          idipol=3,
+                          
+                          # Constant potential parameters
+                          # bpotim=0.1,
+                          # smass=0,
+                          # nelect=100,  # Adjust this to your system's total number of electrons
+                          
+                          # VASP-sol parameters for implicit solvation in water
+                          lsol=True,
+                          eb_k=78.4,
+                          tau=0,
+                          lambda_d_k=3.0,
+                          nc_k=0.0025,
+                          sigma_k=0.6,
+                          
+                          lwave=True,
+                          lcharg=True,
+                          npar=4
+                         )
+        atoms.calc.write_input(atoms = atoms)
+        submit_vasp_calculation(ase_atoms_object=atoms, path_to_files=work,slurm=True,tasks=64,time_cap='01:00:00')
 
-# Set the calculator for the atoms
-atoms.calc = calc
-atoms.calc.write_input(atoms=atoms)
-submit_vasp_calculation(ase_atoms_object=atoms,path_to_files=os.get_cwd(),slurm=False,tasks=32,time_cap='01:00:00', partition = 'genoa')
+
